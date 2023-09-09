94e7b380b2b64f972c8a016b661735c195458e04
zulemarodriguez <136563487+zulemarodriguez@users.noreply.github.com>
2023-09-09 09:03:58 +0200

    Update 02-gwasinspector

--- gwas_qc/02-gwasinspector.sh
+++ gwas_qc/02-gwasinspector.sh
@@ -8,8 +8,8 @@ then
 	exit 3
 fi
 
-IMAGE=/home/mwuttke/SingularityTest/gwasinspector.simg
-MYDIR=/storage/uploads/ckdgenR5/assoc/$STUDY_NAME
+#IMAGE=/home/mwuttke/SingularityTest/gwasinspector.simg
+MYDIR=/data/cne/ec/ieg/Crew/zulema.rodriguez/Consortium_pipeline_core/uploads/assoc/$STUDY_NAME
 
 if [ ! -d "$MYDIR" ]
 then
@@ -20,15 +20,16 @@ fi
 if [ ! -f "$MYDIR/config.ini" ]
 then
 	echo "config.ini not found: Creating one."
-	cat /storage/scripts/ckdgenR5/gwas_qc/GWASinspector/config.ini.template | sed s/STUDY_NAME/$STUDY_NAME/g > $MYDIR/config.ini
+	cat /data/cne/ec/ieg/Crew/zulema.rodriguez/Consortium_pipeline_core/scripts/gwas_qc/GWASinspector/config.ini.template | sed s/STUDY_NAME/$STUDY_NAME/g > $MYDIR/config.ini
 fi
 
-mkdir -p /storage/cleaning/ckdgenR5/$STUDY_NAME/qc-output
+mkdir -p /data/cne/ec/ieg/Crew/zulema.rodriguez/Consortium_pipeline_core/cleaning/$STUDY_NAME/qc-output
 
 echo "Run singularity GWASinspector"
-singularity exec \
-	--bind /storage \
-	$IMAGE \
-	cd $MYDIR Rscript /storage/scripts/ckdgenR5/gwas_qc/GWASinspector/gwasinspector.R \
+#singularity exec \
+#	--bind /storage \
+#	$IMAGE 
+	cd $MYDIR 
+	Rscript /storage/scripts/ckdgenR5/gwas_qc/GWASinspector/gwasinspector.R \
 	2>&1 | tee 02-gwasinspector.log
 
