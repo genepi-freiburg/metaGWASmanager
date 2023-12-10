c07121e1d877d475dc56c44e1683cd6e288a76d3
zulemarodriguez <136563487+zulemarodriguez@users.noreply.github.com>
2023-12-10 19:34:52 +0100

    METAL. Keep only one MAF threshold  as an example

--- metaanalysis/03-prepare-ma-input.HQ.sh
+++ metaanalysis/03-prepare-ma-input.HQ.sh
@@ -25,18 +25,6 @@ echo "HQ_All" | tee $LOG
 mkdir HQ_All
 $SCRIPTS_MA/prepare-ma-input.sh ${MA_P_DIR}/input-files-with-path.txt 0 0.6 1 100 20 0 ${MA_P_DIR}/HQ_All/input_HQ_All $SKIP_EXIST | tee -a $LOG
 
-echo "HQ_MAF1" | tee $LOG
-mkdir HQ_MAF1
-$SCRIPTS_MA/prepare-ma-input.sh ${MA_P_DIR}/input-files-with-path.txt 0.05 0.6 1 100 20 0 ${MA_P_DIR}/HQ_MAF1/input_HQ_MAF1 $SKIP_EXIST | tee -a $LOG
-
-echo "HQ_MAF2" | tee $LOG
-mkdir HQ_MAF2
-$SCRIPTS_MA/prepare-ma-input.sh ${MA_P_DIR}/input-files-with-path.txt 0.005 0.6 1 100 20 0 ${MA_P_DIR}/HQ_MAF1/input_HQ_MAF2 $SKIP_EXIST | tee -a $LOG
-
-echo "HQ_MAF3" | tee $LOG
-mkdir HQ_MAF3
-$SCRIPTS_MA/prepare-ma-input.rare.sh ${MA_P_DIR}/input-files-with-path.txt 0.005 0.8 0 100 20 0 ${MA_P_DIR}/HQ_MAF1/input_HQ_MAF3 $SKIP_EXIST | tee -a $LOG
-
 
 
 
--- metaanalysis/03-prepare-ma-input.LQ.sh
+++ metaanalysis/03-prepare-ma-input.LQ.sh
@@ -26,20 +26,6 @@ mkdir LQ_All
 $SCRIPTS_MA/prepare-ma-input.sh ${MA_P_DIR}/input-files-with-path.txt 0 0.3 1 100 20 0 ${MA_P_DIR}/LQ_All/input_LQ_All $SKIP_EXIST | tee -a $LOG
 
 
-echo "LQ_MAF1" | tee $LOG
-mkdir LQ_MAF1
-$SCRIPTS_MA/prepare-ma-input.sh ${MA_P_DIR}/input-files-with-path.txt 0.05 0.3 1 100 20 0 ${MA_P_DIR}/LQ_All/input_LQ_MAF1 $SKIP_EXIST | tee -a $LOG
-
-echo "LQ_MAF2" | tee $LOG
-mkdir LQ_MAF2
-$SCRIPTS_MA/prepare-ma-input.sh ${MA_P_DIR}/input-files-with-path.txt 0.005 0.3 1 100 20 0 ${MA_P_DIR}/LQ_All/input_LQ_MAF2 $SKIP_EXIST | tee -a $LOG
-
-echo "LQ_MAF3" | tee $LOG
-mkdir LQ_MAF3
-$SCRIPTS_MA/prepare-ma-input.rare.sh ${MA_P_DIR}/input-files-with-path.txt 0.005 0.3 0 100 20 0 ${MA_P_DIR}/LQ_All/input_LQ_MAF3 $SKIP_EXIST | tee -a $LOG
-
-
-
 
 
 
