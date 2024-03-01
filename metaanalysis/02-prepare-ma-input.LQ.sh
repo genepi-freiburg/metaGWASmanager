e0720a3345e0a996641ada8569b3175beb659239
Mathias Gorski <103117148+Mathias0077@users.noreply.github.com>
2024-03-01 15:54:03 +0100

    Update 02-prepare-ma-input.LQ.sh
    LQ_All not needed in scripts dir

--- metaanalysis/02-prepare-ma-input.LQ.sh
+++ metaanalysis/02-prepare-ma-input.LQ.sh
@@ -22,7 +22,7 @@ SKIP_EXIST="$1"
 
 
 echo "LQ_All" | tee $LOG
-mkdir LQ_All
+# mkdir LQ_All
 $SCRIPTS_MA/prepare-ma-input.sh ${MA_P_DIR}/input-files-with-path.txt 0 0.3 1 100 20 0 ${MA_P_DIR}/LQ_All/input_LQ_All $SKIP_EXIST | tee -a $LOG
 
 
