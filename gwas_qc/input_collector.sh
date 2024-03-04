0c843e801a7ed8a1f34121d7ca68ede7f3ef7b04
zulemarodriguez <136563487+zulemarodriguez@users.noreply.github.com>
2024-03-04 12:52:51 +0100

    Remove CKDGen-specific scripts

--- gwas_qc/input_collector.sh
+++
@@ -1,8 +0,0 @@
-for FN in `ls /data/sftp/sftp01`
-do
-	echo "Found submission: $FN"
-#ckdgen-r5-upload-MGBB-221031-AFR.tar
-	SN=$(echo $FN | sed 's/ckdgen-r5-upload-//' | sed 's/.tgz//')
-	echo "Study name: $SN"
-	/storage/scripts/ckdgenR5/gwas_qc/input_pipeline.sh $SN /data/sftp/sftp01/$FN
-done
